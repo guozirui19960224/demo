7dbf63b985f361c271c6234c1a599a515ae7d03f
guozr <zirui.guo@bmlchina.com>
2020-11-17 18:20:55 +0800

    bridge commit

---
+++ src/main/java/com/guozr/demo/designpatterm/bridge/impl/FileDisplayImpl.java
@@ -0,0 +1,58 @@
+package com.guozr.demo.designpatterm.bridge.impl;
+
+import com.guozr.demo.designpatterm.bridge.impl.DisplayImpl;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @ClassName FileDisplayImpl
+ * @Description TODO
+ * @Author guozr
+ * @Date 2020/10/26 11:46
+ **/
+public class FileDisplayImpl extends DisplayImpl {
+    private String filename;
+
+    private BufferedReader bufferedReader;
+
+    private final int MAX_READAHEAD_LIMIT = 4096;
+
+    public FileDisplayImpl(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void rawOpen() {
+        try {
+            bufferedReader = new BufferedReader(new FileReader("G:/demo/src/main/resources/templates/"+filename));
+            bufferedReader.mark(MAX_READAHEAD_LIMIT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(filename);
+    }
+
+    @Override
+    public void rawPrint() {
+        try {
+            String line;
+            bufferedReader.reset();
+            while((line = bufferedReader.readLine()) != null){
+                System.out.println(">:"+line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void rawClose() {
+        try {
+            bufferedReader.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
