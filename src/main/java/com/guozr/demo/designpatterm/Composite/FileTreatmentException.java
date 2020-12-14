4d9a325daadf59240048609593fd858e2ab94e82
guozr <zirui.guo@bmlchina.com>
2020-12-14 15:02:53 +0800

    composite commit

---
+++ src/main/java/com/guozr/demo/designpatterm/Composite/FileTreatmentException.java
@@ -0,0 +1,17 @@
+package com.guozr.demo.designpatterm.Composite;
+
+/**
+ * @ClassName FileTreatementException
+ * @Description TODO
+ * @Author guozr
+ * @Date 2020/10/27 18:02
+ **/
+public class FileTreatmentException extends RuntimeException {
+
+    public FileTreatmentException() {
+    }
+
+    public FileTreatmentException(String message) {
+        super(message);
+    }
+}
